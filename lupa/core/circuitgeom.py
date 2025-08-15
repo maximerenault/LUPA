0db44d67908a96a000d0b5fb8af0b4dc1f27dea8
maximerenault <maxime.renault@minesparis.psl.eu>
2025-08-15 23:37:42 +0200

    circuitgeom.py: remove deprecated binary searches

--- lupa/core/circuitgeom.py
+++ lupa/core/circuitgeom.py
@@ -1,4 +1,3 @@
-from lupa.elements.node import Node
 from lupa.elements.wire import Wire
 
 
@@ -32,65 +31,3 @@ class CircuitGeom:
         """
         self.elems.clear()
         self.nodes.clear()
-
-    ##
-    # DEPREC
-    ##
-
-    def binary_search_node(self, low: int, high: int, node: Node) -> tuple[int, bool]:
-        """
-        Returns the index of the node for insertion,
-        or the index of the existing node if exists==True.
-
-        Actually useless because sorted+bisect does the same thing.
-        """
-        index = 0
-        exists = False
-        nnodes = len(self.nodes)
-        if nnodes == 0:
-            index = 0
-            exists = False
-        elif high >= low:
-            mid = (high + low) // 2
-            snode = self.nodes[mid]
-            if snode == node:
-                index = mid
-                exists = True
-            elif snode > node:
-                return self.binary_search_node(low, mid - 1, node)
-            else:
-                return self.binary_search_node(mid + 1, high, node)
-        else:
-            index = low
-            exists = False
-        return index, exists
-
-    def binary_search_elem(self, low: int, high: int, elem: Wire) -> tuple[int, bool]:
-        """
-        Returns the index of the elem for insertion,
-        or the index of the existing elem if exists==True.
-        Normally elems are created in order so there shouldn't be any use for the first
-        option.
-        """
-        index = 0
-        exists = False
-        selems = self.elems
-        nelems = len(selems)
-        if nelems == 0:
-            index = 0
-            exists = False
-        elif high >= low:
-            mid = (high + low) // 2
-            sid = selems[mid].ids[0]
-            id = elem.ids[0]
-            if sid == id:
-                index = mid
-                exists = True
-            elif sid > id:
-                return self.binary_search_elem(low, mid - 1, elem)
-            else:
-                return self.binary_search_elem(mid + 1, high, elem)
-        else:
-            index = low
-            exists = False
-        return index, exists
